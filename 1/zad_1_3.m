a5d6ac50c8ea6366b0afff3370575b92c1275f7d
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2021-09-14 13:05:14 +0300

    лаба 1

---
+++ 1/zad_1_3.m
@@ -0,0 +1,16 @@
+str1 = "Столяров";
+str2 = "Никита";
+str3 = "Сергеевич";
+
+str4 = strcat(str1, str2, str3);
+str5 = [str1, str2, str3];
+
+a_pos = strfind(str4, "а")
+
+if (length(strfind(str4, "ю")) > 0)
+    disp("В ФИО есть буква Ю"); 
+else
+    disp("В ФИО нет буквы Ю");
+end
+
+lowercase(str4)
