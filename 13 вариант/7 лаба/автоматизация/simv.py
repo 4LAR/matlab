9941994a6ce2b563025e29501a5297c8d6b49266
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2021-12-23 13:51:37 +0300

    почти 2 часть 7 лабы

--- 13 вариант/7 лаба/автоматизация/simv.py
+++ 13 вариант/7 лаба/автоматизация/simv.py
@@ -5,18 +5,24 @@ import os
 slovar = []
 count = []
 
+f2_text = ''
+
 slovar_f = open('slovar.txt', 'r', encoding="utf-8")
 for s in slovar_f.read():
     slovar.append(s)
     count.append(0)
     
 f = open('text.txt', 'r', encoding="utf-8")
+f2 = open('text2.txt', 'w', encoding="utf-8")
 
 for c in f.read():
     for i in range(len(slovar)):
         if c == slovar[i]:
             count[i] += 1
+            f2_text += c
 
+f2.write(f2_text)
+f2.close()
 for i in range(len(slovar)):
     print(slovar[i], count[i])
 
